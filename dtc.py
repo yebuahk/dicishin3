5502012c202b880272ca0d9aad59fa2204dc45ac
yebuahk <kevin.yebuah@gmail.com>
2019-07-31 05:50:27 -0700

    tree

--- dtc.py
+++ dtc.py
@@ -27,9 +27,45 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
 from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier()
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 
 X_train.shape
 
 y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
+cm = confusion_matrix(y_pred, y_test)
+cm
+
+pd.crosstab(y_pred, y_test)
+
+total = sum(sum(cm))
+
+accuracy = (cm[0, 0]+cm[1, 1])/total
+print('Accuracy :', accuracy)
+classifier.score(X_test, y_test)    # calculate accuracy automatically instead of manually
+
+sensitivity = cm[0, 0]/(cm[0, 0]+cm[0, 1])
+print('Sensitivity :', sensitivity)
+
+specificity = cm[1, 1]/(cm[1, 0]+cm[1, 1])
+print('specificity :', specificity)
+
+precision = cm[0, 0]/(cm[0, 0]+cm[1, 0])
+print('precision :', precision)
+
+print(classification_report(y_test, y_pred))
+
+dtree = DecisionTreeClassifier(criterion='gini', max_leaf_nodes=10, min_samples_leaf=5, max_depth=5)
+dtree.fit(X_train, y_train)
+
+predict3 = dtree.predict(X_test)
+score2 = dtree.score(X_test, y_test)
+score2
+
+from sklearn import tree
+from IPython.display import Image
+
+tree.export_graphviz(classifier, out_file='C:/Users/kevin/PycharmProjects/dicishin3/wp.png')
+Image(filename='C:/Users/kevin/PycharmProjects/dicishin3/wp.png')
